81e56e005e543e3c3443842e4e1281b15861d2f3
Chase Struse <chasestruse@Chases-MacBook-Pro.local>
2022-01-10 11:00:09 -0600

    WIP, currently getting invalid opcode error

--- test/User.test.js
+++ test/User.test.js
@@ -10,10 +10,13 @@ require('chai')
 contract('User', ([deployer, seller, buyer]) => { 
     let storage;
     let user;
+    let product;
 
     before (async () => {
         storage = await Storage.deployed();
         user = await User.deployed();
+        storage.addProduct('Macbook', web3.utils.toWei('1'), 1, { from: seller });
+        product = await storage.getProductByOwner(seller, 0);
     });
     
     describe('deployment', async() => {
@@ -22,4 +25,12 @@ contract('User', ([deployer, seller, buyer]) => {
             assert.notEqual(userAddress, 0x0);
         })
     });
+
+    describe('purchasing', async() => {
+        it('transfers ownership', async() => {
+            user.PurchaseProduct(seller, 0, { from: buyer, value: web3.utils.toWei('1', 'Ether') });
+            const updatedProduct = await storage.getProductByOwner(buyer, 0);
+            assert.equal(updatedProduct.owner = buyer);
+        })
+    })
 });
